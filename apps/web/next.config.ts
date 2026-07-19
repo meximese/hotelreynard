488d01b85e49bfc3472bb619cfa72a3a1ab27853
Vinh Nguyen <meximese@gmail.com>
2026-07-19 10:27:33 -0700

    had to add allowedDevOrigins to get js working on mobile local

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import path from "node:path";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["10.0.0.5"],
   images: {
     remotePatterns: [
       {
